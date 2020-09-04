d46b23f4c62e5c2b1f057b356c4d9ad6a40bb054
18wh1a1222 <18wh1a1222@bvrithyderabad.edu.in>
2020-09-04 13:17:34 +0530

    validate password

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,9 +8,13 @@ import { RouteReuseStrategy, Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  confirmPassword: any;
+  customer : any;
+  volunteer: any;
 
-  constructor(private service: SahaayService, private router: Router) { }
+  constructor(private service: SahaayService, private router: Router) {
+    this.customer = {customerId:'',age:'',covidAssess:'', customerName:'', gender:'', govId:'', latitude:'', longitude:'', loginId: '', password:'', phone:''};
+    this.volunteer = {volunteerId: '', volunteerName:'', age:'', phone:'', loginId:'', password:'', ngoName:'', isDonor:'', isVolunteer:'', covidAssess:''};  
+ }
 
   ngOnInit(): void {
   }
@@ -19,9 +23,17 @@ export class RegisterComponent implements OnInit {
     this.router.navigate(['login']);
   }
   registerCustomer(registerCustomerForm: any): void {
-    if (registerCustomerForm.password == this.confirmPassword) {
-      this.service.registerCustomer(registerCustomerForm).subscribe((result: any) => { console.log(result); })
-      console.log(registerCustomerForm);
+    console.log(registerCustomerForm.confirmPassword);
+    if (registerCustomerForm.password === registerCustomerForm.confirmPassword) {
+      this.customer.age = registerCustomerForm.age;
+      this.customer.customerName = registerCustomerForm.customerName;
+      this.customer.gender = registerCustomerForm.gender;
+      this.customer.govId = registerCustomerForm.govId;
+      this.customer.loginId = registerCustomerForm.loginId;
+      this.customer.password = registerCustomerForm.password;
+      this.customer.phone = registerCustomerForm.phone;
+      this.service.registerCustomer(this.customer).subscribe((result: any) => { console.log(result); })
+      console.log(this.customer);
       this.router.navigate(['login']);
     }
     else {
@@ -31,13 +43,21 @@ export class RegisterComponent implements OnInit {
   }
 
   registerVolunteer(registerVolunteerForm: any): void {
-    if (registerVolunteerForm.password == this.confirmPassword) {
-      this.service.registerVolunteer(registerVolunteerForm).subscribe((result: any) => { console.log(result); })
-      console.log(registerVolunteerForm);
+    if (registerVolunteerForm.password == registerVolunteerForm.confirmPassword) {
+      this.volunteer.volunteerName = registerVolunteerForm.volunteerName;
+      this.volunteer.age = registerVolunteerForm.age;
+      this.volunteer.phone = registerVolunteerForm.phone;
+      this.volunteer.loginId = registerVolunteerForm.loginId;
+      this.volunteer.password = registerVolunteerForm.password;
+      this.volunteer.ngoName = registerVolunteerForm.ngoName;
+      this.volunteer.isVolunteer = registerVolunteerForm.isVolunteer;
+      this.volunteer.isDonor = registerVolunteerForm.isDonor;
+      this.service.registerVolunteer(this.volunteer).subscribe((result: any) => { console.log(result); })
+      console.log(this.volunteer);
       this.router.navigate(['login']);
     }
     else {
-      alert(this.confirmPassword);
+      alert("Password not Same");
       this.router.navigate(['register']);
     }
   }
